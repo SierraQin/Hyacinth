45714ba4e2763cace0f81bd523b49ddff4721c91
SierraQin <sierra@qinxr.cn>
2023-12-02 03:50:13 +0800

    优化底纹样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     //cardList: [true, false, false, false, false],
     cardList: [true, true, true, true, true],
     cardBgList: localData.cardBgB64,
-    cardTitleList: ["路网配线图", "路网配线图(旧版)", "站牌生成器", "反馈问题", "关于"],
+    cardTitleList: ["路网配线图", "路网配线图(旧)", "站牌生成器", "反馈问题", "关于"],
     cardTextList: [
       "基于Skyline渲染引擎重构，缩放丝滑不卡顿",
       "旧版小程序，专为Mac及老版本微信保留",
