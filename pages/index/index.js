8943ee5bb8b0d96d5afe126990f4e7c9332d65e3
SierraQin <sierra@qinxr.cn>
2023-12-01 21:09:47 +0800

    首页草稿

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,32 @@
 // pages/index/index.js
 
-Page({});
+Page({
+  data: {
+    cardTitleList: ["路网配线图", "路网配线图(旧版)", "站牌生成器", "反馈问题", "关于"],
+    cardTextList: [
+      "基于Skyline渲染引擎重构，缩放丝滑不卡顿",
+      "旧版小程序，专为Mac及老版本微信保留",
+      "无功能，占位",
+      "无功能，占位",
+      "无功能，占位"
+    ],
+    cardIconList: ["map-collection", "map-cancel", "indicator", "bug", "info-circle"],
+    cardUrlList: [
+      "/pages/proj853/index",
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+
+  cardNaviHandler(evt) {
+    const that = this;
+    var idx = parseInt(evt.currentTarget.id.slice(5));
+    wx.navigateTo({
+      url: that.data.cardUrlList[idx]
+    });
+  }
+
+});
